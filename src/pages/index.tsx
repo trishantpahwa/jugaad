608eb51f4cc0ed8487376ad10304ef8964ffcf6b
Trishant Pahwa <trishantpahwa@gmail.com>
2024-03-17 15:00:45 +0530

    Fixed type issue in index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   const [accessToken, setAccessToken] = useState("");
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<any>(null);
 
   const signIn = async () => {
     const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
