894fcf9c1bef2a5f28886dde115e7628259b8e48
Trishant Pahwa <trishantpahwa@gmail.com>
2024-09-14 12:40:10 +0530

    Updated post-checkout hook to check if branch exists on remote.

--- .husky/pull_branch.sh
+++ .husky/pull_branch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-read -p "Do you want to pull the latest changes from the current branch? (y/n): " answer < /dev/tty
-if [[ $answer =~ ^[Yy]$ ]]
-then
-    echo "Pulling latest changes from the current branch."
-    branch=$(git branch --show-current)
-    git pull origin $branch
+
+branch=$(git branch --show-current)
+remote_branch_exists=$(git ls-remote --exit-code --heads origin $branch >/dev/null 2>&1; echo $?)
+
+if [[ $remote_branch_exists -eq 0 ]]; then
+    read -p "Do you want to pull the latest changes from the current branch? (y/n): " answer < /dev/tty
+    if [[ $answer =~ ^[Yy]$ ]]; then
+        echo "Pulling latest changes from the current branch."
+        git pull origin $branch
+    else
+        echo "Skipping pull."
+    fi
 else
-    echo "Skipping pull."
+    echo "The branch $branch does not exist on the remote server."
 fi
